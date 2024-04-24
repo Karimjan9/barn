40ebf0b7d40e853d205312535eec42e8b4d79484
Karimjan <karimjonmirzayev199@gamil.com>
2024-04-24 18:35:02 +0500

    update

--- database/seeders/UnityItemsSeeder.php
+++ database/seeders/UnityItemsSeeder.php
@@ -8,11 +8,7 @@ use Illuminate\Database\Seeder;
 
 class UnityItemsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+  
     public function run()
     {
        
