7018c79171c981659dbf61e1f9bc282d96270217
Karimjan <karimjonmirzayev199@gamil.com>
2024-05-30 15:07:02 +0500

    check with dd_3

--- app/Http/Controllers/Barn/Rektor/RektorPrixodController.php
+++ app/Http/Controllers/Barn/Rektor/RektorPrixodController.php
@@ -13,34 +13,31 @@ class RektorPrixodController extends Controller
   
     public function index()
     {
-        $cargos=CargoModel::with(['child'])->orderBy('id', 'DESC')->paginate(40);
-       
-        $all_inf=[];
-        foreach ($cargos as $key => $cargo) {
-            dd($cargo); 
-            $prixod_num=0;
-            $prixod_cost=0;
-            $inf=[];
-            $prixod_curer=[];
-            $prixod_currency_cost_all=0;
+        $cargos=CargoModel::with(['child'])->orderBy('id', 'DESC')->paginate(30);
+            // dd($cargos);
+            $all_inf=[];
+            foreach ($cargos as $key => $cargo) {
+                $prixod_num=0;
+                $prixod_cost=0;
+                $prixod_currency_cost_all=0;
+                $inf=[];
+                $prixod_curer=[];
+                // $prixods=PrixodModel::with(['get_provider'])->where('cargo_id',$cargo->id)->get();
+                foreach ($cargo->child as $key => $prixod) {
+                      $prixod_num+=$prixod->count_of_item; 
+                      $prixod_cost+=$prixod->cost_of_per*$prixod->count_of_item*$prixod->currency_value;
+                      $prixod_currency_cost=$prixod->cost_of_per*$prixod->count_of_item;
+                      $prixod_currency_cost_all=$prixod_currency_cost_all+$prixod_currency_cost;
+                      $prixod_curer=$prixod->get_provider->name?? "kiritilmagan";
+                 
+                }
+                $inf[]=(int)$prixod_cost;
+                $inf[]=$prixod_num;
+                $inf[]=$prixod_curer;
+                $inf[]=$prixod_currency_cost_all??0;
 
-           
-            foreach ($cargo->child as $key => $prixod) {
-               
-                  $prixod_num+=$prixod->count_of_item; 
-                  $prixod_cost+=$prixod->cost_of_per*$prixod->count_of_item;
-                  $prixod_curer=$prixod->get_provider->name?? "kiritilmagan";
-                  dd($prixod->get_provider->name);
-                  $prixod_currency_cost=$prixod->cost_of_per*$prixod->count_of_item*$prixod->currency_value;
-                  $prixod_currency_cost_all=$prixod_currency_cost_all+$prixod_currency_cost;
+                $all_inf[]=$inf;
             }
-            $inf[]=$prixod_cost;
-            $inf[]=$prixod_num;
-            $inf[]=$prixod_curer;
-            dd($prixod_curer);
-            $inf[]=$prixod_currency_cost_all??0;
-            $all_inf[]=$inf;
-        }
             // dd(1);
         return view('barn.rektor.prixods.index',compact('cargos','all_inf'));
     }
